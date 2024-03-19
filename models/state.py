4cd8e1df771c13c6a7d7a1549e2a88a1d20e5738
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-03-19 12:55:52 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
