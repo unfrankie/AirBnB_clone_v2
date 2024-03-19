da1faa8b809465d68117052920b8958a40c8766b
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-03-19 14:42:11 +0000

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -13,6 +13,7 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     if storage_type == "db":
         name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary='place_amenity', viewonly=False)
+        place_amenities = relationship('Place', secondary='place_amenity',
+                                       back_populates='amenities')
     else:
         name = ""
