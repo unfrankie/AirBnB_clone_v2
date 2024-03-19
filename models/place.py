06c3a4a84b8a97d3a3023b82139e2dc643f79f1f
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-03-19 14:36:58 +0000

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
+
+if storage_type == "db":
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60), ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False)
+                          )
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -24,6 +32,7 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         reviews = relationship('Review', cascade="all,delete", backref="place")
+        amenities = relationship('Amenity', secondary='place_amenity', viewonly=False)
     else:
         city_id = ""
         user_id = ""
