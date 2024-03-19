78d75ece4fdeb62df9eacd464517d8ddc4f168a3
Anass El Omari <anas.iramol@gmail.com>
2024-03-19 04:52:45 -0700

    add comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,9 @@
+-- Create or use the hbnb_dev_db database
+-- Create or update the hbnb_dev user with the specified password
+-- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
+-- Grant SELECT privilege on the performance_schema database to the hbnb_dev user
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
