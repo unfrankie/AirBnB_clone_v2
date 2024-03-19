98566ba8d04804b81e15de8f3e09efbd719c0240
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-03-19 12:54:11 +0000

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,13 +3,9 @@
 from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from models.base_model import Base
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.user import User
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class DBStorage:
