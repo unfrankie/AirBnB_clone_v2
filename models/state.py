8a5518c4615bf78298da9a2733f25205cc1cd190
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-03-19 13:52:35 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.city import City
 from os import getenv
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
@@ -10,15 +11,12 @@ storage_type = getenv("HBNB_TYPE_STORAGE")
 
 class State(BaseModel, Base):
     """ State class """
-
     __tablename__ = 'states'
     if storage_type == "db":
         name = Column(String(128), nullable=False)
         cities = relationship('City', cascade="all,delete", backref="state")
-
     else:
         name = ""
-
         @property
         def cities(self):
             """getter for cities related to this state"""
