d9e374cffdac2bf7ba376ad3005804a8d7c3c61b
Anass El Omari <anas.iramol@gmail.com>
2024-03-19 05:20:57 -0700

    5. Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is None:
             return FileStorage.__objects
@@ -52,7 +52,12 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    class_name = val['__class__']
+                    if class_name in classes:
+                        if 'id' in val:
+                            obj = classes[class_name](**val)
+                            key = "{}.{}".format(class_name, val['id'])
+                            FileStorage.__objects[key] = obj
         except FileNotFoundError:
             pass
 
