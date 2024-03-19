d00c9f2bcd0a255c05b99306d81fe0ae3e778646
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-03-19 13:07:32 +0000

    Update city.py

--- models/city.py
+++ models/city.py
@@ -11,5 +11,10 @@ storage_type = getenv("HBNB_TYPE_STORAGE")
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship('Place', cascade="all,delete", backref="cities")
+    else:
+        name = ""
+        state_id = ""
