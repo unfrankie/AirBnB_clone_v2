b5758ade41081d3e103c7ddc4309e5ab9fac9d02
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-03-19 13:48:57 +0000

    Update city.py

--- models/city.py
+++ models/city.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
+from models.place import Place
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
