8f79f6fc62bfb96ca1c5ae5e28c847bc94d05147
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-03-19 12:58:59 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
