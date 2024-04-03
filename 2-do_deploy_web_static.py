61f687f010aa738134a9b29c73b93b8f6e911d18
Anass El Omari <anas.iramol@gmail.com>
2024-04-03 13:17:30 -0700

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,26 +2,30 @@
 """Module to deploy an archive to web servers"""
 from fabric.api import env, run, put
 from os.path import exists
-
-
 env.hosts = ['54.197.50.4', '34.229.137.21']
+env.user = 'ubuntu'
+
+
 def do_deploy(archive_path):
     """Distributes an archive to web servers"""
     if not exists(archive_path):
         return False
-
     try:
         filename = archive_path.split('/')[-1]
-        folder = '/data/web_static/releases/' + filename[:-4]
-
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(folder))
-        run('tar -xzf /tmp/{} -C {}'.format(filename, folder))
-        run('rm /tmp/{}'.format(filename))
-        run('mv {}/web_static/* {}/'.format(folder, folder))
-        run('rm -rf {}/web_static'.format(folder))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(folder))
-        return True
-    except:
+        no_folder = filename.split(".")[0]
+        folder = "/data/web_static/releases/{}".format(no_folder)
+        if (
+            put(archive_path, '/tmp/').failed or
+            run('mkdir -p {}/'.format(folder)).failed or
+            run('tar -xzf /tmp/{} -C {}/'.format(file_name, folder)).failed or
+            run('rm /tmp/{}'.format(file_name)).failed or
+            run('mv {}/web_static/* {}/'.format(folder, folder)).failed or
+            run('rm -rf {}/web_static'.format(folder)).failed or
+            run('rm -rf /data/web_static/current').failed or
+            run('ln -s {}/ /data/web_static/current'.format(folder)).failed
+        ):
+            return False
+    except Exception:
         return False
+    print("New version deployed!")
+    return True
