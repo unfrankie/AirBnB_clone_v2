b87d1099a2fc830ed2c43083216dd4d908367d49
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-04 19:44:56 +0000

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,13 +1,46 @@
 #!/usr/bin/python3
 """Module to perform full deployment"""
-from fabric.api import *
+from fabric.api import env, run, put, local
 from datetime import datetime
-from os.path import exists, isdir
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
+from os.path import exists
 env.hosts = ['54.197.50.4', '34.229.137.21']
 env.user = 'ubuntu'
 
+
+def do_pack():
+    """Create a tgz archive from web_static content"""
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(timestamp)
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf {} web_static".format(filename))
+        return filename
+    except:
+        return None
+
+def do_deploy(archive_path):
+    """Distributes an archive to web servers"""
+    if not exists(archive_path):
+        return False
+    try:
+        filename = archive_path.split('/')[-1]
+        no_folder = filename.split(".")[0]
+        folder = "/data/web_static/releases/{}".format(no_folder)
+        if (
+            put(archive_path, '/tmp/').failed or
+            run('mkdir -p {}/'.format(folder)).failed or
+            run('tar -xzf /tmp/{} -C {}/'.format(filename, folder)).failed or
+            run('rm /tmp/{}'.format(filename)).failed or
+            run('mv {}/web_static/* {}/'.format(folder, folder)).failed or
+            run('rm -rf {}/web_static'.format(folder)).failed or
+            run('rm -rf /data/web_static/current').failed or
+            run('ln -s {}/ /data/web_static/current'.format(folder)).failed
+        ):
+            return False
+    except Exception:
+        return False
+    print("New version deployed!")
+    return True
 
 def deploy():
     """Creates and distributes an archive to web servers"""
