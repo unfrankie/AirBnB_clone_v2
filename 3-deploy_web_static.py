a95c87d453bb2314ed28fc8a397bb074444fca2b
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-04 19:40:02 +0000

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -2,8 +2,11 @@
 """Module to perform full deployment"""
 from fabric.api import *
 from datetime import datetime
+from os.path import exists, isdir
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
+env.hosts = ['54.197.50.4', '34.229.137.21']
+env.user = 'ubuntu'
 
 
 def deploy():
