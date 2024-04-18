9c708bf4163b16b83019309344c438e56374b411
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-18 19:45:35 +0100

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -7,11 +10,13 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_db(exception):
+    """Closes the current session"""
     storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
+    """Displays a list of states"""
     states = storage.all(State).values()
     sorted_states = sorted(states, key=lambda x: x.name)
     return render_template('7-states_list.html', states=sorted_states)
