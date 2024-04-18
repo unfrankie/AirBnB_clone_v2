b9fe1032474ca114fada53a3bfe23b6d4c9cb961
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-18 19:29:20 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,7 @@ class FileStorage:
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         if key in FileStorage.__objects:
             del FileStorage.__objects[key]
+
+    def close(self):
+        """Call reload method for deserializing the JSON file to objects"""
+        self.reload()
