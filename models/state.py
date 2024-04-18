4590a89b260fe2c5650f44ae96d9e55875072cd2
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-18 19:27:16 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -16,8 +16,6 @@ class State(BaseModel, Base):
         name = Column(String(128), nullable=False)
         cities = relationship('City', cascade="all,delete", backref="state")
     else:
-        name = ""
-
         @property
         def cities(self):
             """Getter for cities related to this state"""
