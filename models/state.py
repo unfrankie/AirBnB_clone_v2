12b10655d2408da8c430fbf88f132d15a718b2e3
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-19 14:15:06 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -11,14 +11,17 @@ storage_type = getenv("HBNB_TYPE_STORAGE")
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
     if storage_type == "db":
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship('City', cascade="all,delete", backref="state")
+        cities = relationship('City', backref="state")
     else:
         @property
         def cities(self):
             """Getter for cities related to this state"""
-            from models import storage
-            cities_list = [city for city in storage.all(City).values() if city.state_id == self.id]
+            city_list = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
             return cities_list
