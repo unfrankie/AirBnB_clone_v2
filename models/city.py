5b80395efaf798bf41c0abc0305048b0d5b3f31d
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-19 17:25:08 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import models
+import sqlalchemy
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class City(BaseModel, Base):
-    """Class City"""
-
-    __tablename__ = "cities"
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
     if storage_type == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship('Place', cascade="all,delete", backref="cities")
-
+        places = relationship('Place', backref="cities", cascade='all, delete')
     else:
         name = ""
         state_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
