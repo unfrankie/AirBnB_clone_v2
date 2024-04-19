7bfc7affe9edd80e4ec9baf07907b4caa03e0f5b
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-19 15:41:44 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+import models
+import sqlalchemy
 from models.base_model import BaseModel, Base
 from models.review import Review
 from models.amenity import Amenity
