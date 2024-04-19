a5ff0883c2fe6f8ed7a39d16302e9b74c47aa5ed
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-19 16:22:27 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -16,7 +16,7 @@ class City(BaseModel, Base):
     if storage_type == "db":
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship('Place', backref="cities")
+        places = relationship('Place', backref="cities", cascade='all, delete')
     else:
         name = ""
         state_id = ""
